4cd64ab012de2ad463abb199102bb14f202bbfa1
evikerve@gmail.com <evikerve@gmail.com>
2021-11-09 13:20:06 +0200

    .

--- PythinApp1.py
+++ PythinApp1.py
@@ -1,11 +1,6 @@
 from keyboard import*
 from random import*
-def start()
-"""Teeme valik kellega mängime
-Tagastame m muutuja int formaadis
-
-:rtype: int
-"""
+def start():
 print("Kivi, Käärid, Paber")
 m=3
 while m not in [1,2]:
@@ -14,7 +9,21 @@ while m not in [1,2]:
     except:
         ValueError
 return m
+def bot_vs_bot():
 m=start()
 if m==1:
     while True:
-        print("Kas ")
+        print("Kas mängime? esc - välja, enter - mängime")
+        if read_key()=="esc":
+            break
+        elif read_key()=="enter":
+            p1=choice(v1)
+            print("Esimene bot: ",p1)
+            p2=choice(v2)
+            print("Teine bot: ",p2)
+            if p1==p2:
+                print("Viik")
+            elif p1==v1[0] and p2==v2[1] or p1==v1[2] and p2==v2[0] or p1==v1[1] and p2==v2[2]:
+                print("Võttis 1")
+            else:
+                print("Võttis 2")
