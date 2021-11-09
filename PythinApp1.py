e1c37e42f55feaf3f98528ba00eba09e31e03a4d
evikerve@gmail.com <evikerve@gmail.com>
2021-11-09 13:02:49 +0200

    .

--- PythinApp1.py
+++ PythinApp1.py
@@ -1,1 +1,20 @@
-print("Камень, ножницы, бумага")
+from keyboard import*
+from random import*
+def start()
+"""Teeme valik kellega mängime
+Tagastame m muutuja int formaadis
+
+:rtype: int
+"""
+print("Kivi, Käärid, Paber")
+m=3
+while m not in [1,2]:
+    try:
+        m=int(input("Kellega mängimine?\n1-botid \n2-robotiga"))
+    except:
+        ValueError
+return m
+m=start()
+if m==1:
+    while True:
+        print("Kas ")
